f4ec7f86a26d5111e5efd5dc2a8cf2ae88c6638c
artur.sokalski <artur.sokalski@zaven.pl>
2025-05-15 15:11:52 +0200

    feature: Added basic style setup and initial navbar configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [react()]
+  integrations: [react()],
+  i18n: {
+    locales: ['en', 'pl'],
+    defaultLocale: 'pl'
+  }
 });
