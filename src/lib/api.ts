a29169aef068cd5a9c46f9fc580f4c782d36dc3c
tipiso <tipiso@gmail.com>
2025-05-23 21:44:00 +0200

    feature: Added product page

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,38 +1,41 @@
 import { API_URL } from "astro:env/client";
 
 type GraphQLResponse<T> = {
-    data: T;
-    errors?: { message: string }[];
-  };
-  
-  export class WPGraphQLClient {
-    private baseURL: string;
-    private headers: HeadersInit;
-  
-    constructor(baseURL: string, headers: HeadersInit = {}) {
-      this.baseURL = baseURL;
-      this.headers = {
-        'Content-Type': 'application/json',
-        ...headers,
-      };
-    }
-  
-    async post<T>(query: string, variables: Record<string, any> = {}): Promise<T> {
-      const response = await fetch(this.baseURL, {
-        method: 'POST',
-        headers: this.headers,
-        body: JSON.stringify({ query, variables }),
-      });
-  
-      const json: GraphQLResponse<T> = await response.json();
-  
-      if (json.errors) {
-        console.error("GraphQL errors:", json.errors);
-        throw new Error(json.errors.map(e => e.message).join(", "));
-      }
+  data: T;
+  errors?: { message: string }[];
+};
 
-      return json.data;
-    }
+export class WPGraphQLClient {
+  private baseURL: string;
+  private headers: HeadersInit;
+
+  constructor(baseURL: string, headers: HeadersInit = {}) {
+    this.baseURL = baseURL;
+    this.headers = {
+      "Content-Type": "application/json",
+      ...headers,
+    };
   }
 
-  export default new WPGraphQLClient(`${API_URL}/graphql`);
+  async post<T>(
+    query: string,
+    variables: Record<string, any> = {}
+  ): Promise<T> {
+    const response = await fetch(this.baseURL, {
+      method: "POST",
+      headers: this.headers,
+      body: JSON.stringify({ query, variables }),
+    });
+
+    const json: GraphQLResponse<T> = await response.json();
+
+    if (json.errors) {
+      console.error("GraphQL errors:", json.errors);
+      throw new Error(json.errors.map((e) => e.message).join(", "));
+    }
+
+    return json.data;
+  }
+}
+
+export default new WPGraphQLClient(`${API_URL}/graphql`);
--- src/lib/products.ts
+++ src/lib/products.ts
@@ -2,73 +2,75 @@ import api from "./api";
 
 /** Products List Response */
 type Photo = {
-    filePath: string;
-}
+  filePath: string;
+};
 
 type Product = {
-    id: string,
-    heroPhoto: { node: Photo },
-    heroAltText: string
-}
+  id: string;
+  heroPhoto: { node: Photo };
+  heroAltText: string;
+};
 
 type ProductsResponse = {
-    products: {
-        nodes: Product[]
-    }
-}
+  products: {
+    nodes: Product[];
+  };
+};
 
 /** Single Product Response */
 
 export interface Data {
-    product: Product;
+  product: Product;
 }
 
 export interface SingleProductType {
-    id:                  string;
-    heroHeader:          string;
-    heroAltText:         string;
-    heroText:            string;
-    introductionAltText: string;
-    introductionHeader:  string;
-    introductionText:    string;
-    introductionPhoto:   Photo;
-    moreDetailsAltText:  string;
-    moreDetailsHeader:   string;
-    moreDetailsText:     string;
-    moreDetailsPhoto:    null;
-    heroPhoto:           Photo;
+  id: string;
+  heroHeader: string;
+  heroAltText: string;
+  heroText: string;
+  introductionAltText: string;
+  introductionHeader: string;
+  introductionText: string;
+  introductionPhoto: SingleProductPhoto;
+  moreDetailsAltText: string;
+  moreDetailsHeader: string;
+  moreDetailsText: string;
+  moreDetailsPhoto: SingleProductPhoto | null;
+  heroPhoto: SingleProductPhoto;
 }
 
 export interface SingleProductPhoto {
-    node: {
-        altText: string;
-        srcSet:  string;
-        title:   string;
-    };
+  node: {
+    altText: string;
+    srcSet: string;
+    title: string;
+  };
 }
 
 type SingleProductResponse = {
-    data: {
-        product: SingleProductType
-    }
-}
+  data: {
+    product: SingleProductType;
+  };
+};
 
-export const getProducts = async (limit:number = 3) => api.post<ProductsResponse>(`
-query Fetch3FirstProducts {
-  products(first: ${limit}) {
-    nodes {
-      id
-      heroPhoto {
-        node {
-          filePath
+export const getProducts = async (limit: number = 3) =>
+  api.post<ProductsResponse>(`
+  query Fetch3FirstProducts {
+    products(first: ${limit}) {
+      nodes {
+        id
+        heroPhoto {
+          node {
+            filePath
+          }
         }
+        heroAltText
       }
-      heroAltText
     }
-  }
 }`);
 
-export const getSingleProduct = async (productId:string) => api.post<SingleProductResponse>(`query GetSingleProduct {
+export const getSingleProduct = async (productId: string) =>
+  api.post<SingleProductResponse>(`query GetSingleProduct {
   product(id: ${productId}) {
     id
     heroHeader
